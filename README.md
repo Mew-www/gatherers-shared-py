4b9ec17ce312f7e28696e0f51da230e211a14606
Mew-www <mew.proxy@hotmail.com>
2020-08-15 16:55:54 +0300

    Initial commit with setup.py

---
+++ README.md
@@ -0,0 +1,1 @@
+Utilities for gathering services. Previously nameko-based utils, now aim to keep requirements to absolute minimum.  
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    install_requires = list(fh.read().splitlines())
+
+setuptools.setup(
+    name="gatherers-shared-py",
+    version="1.0.0",
+    author="Miy",
+    author_email="mew.proxy@hotmail.com",
+    description="Utilities for gathering services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mew-www/gatherers-shared-py",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ]
+)
