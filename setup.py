572caf5e9e8e010d4c8279fc235623861b1f8bb8
Mew-www <mew.proxy@hotmail.com>
2020-08-16 19:52:21 +0300

    Fix delayed import (only supported with __future__ annotations)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="gatherers-shared-py",
-    version="1.2.0",
+    version="1.2.1",
     author="Miy",
     author_email="mew.proxy@hotmail.com",
     description="Utilities for gathering services",
